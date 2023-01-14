9d23a33ee4e9dab493f17c65929f4ba619b5ceed
Renegade-Master <renegade.master.dev@protonmail.com>
2023-01-14 15:53:48 +0000

    Update libraries;
    - gradle/wrapper/gradle-wrapper.properties
      - Update Gradle to 7.6.
    
    - build.gradle.kts
      - Update JetBrains Compose to 1.2.2.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.7.20"
-    id("org.jetbrains.compose") version "1.2.1"
+    id("org.jetbrains.compose") version "1.2.2"
 }
 
 group = "com.renegademaster"
