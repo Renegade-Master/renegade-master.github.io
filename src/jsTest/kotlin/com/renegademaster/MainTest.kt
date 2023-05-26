ef06bc1edc783129d37d5c551132ef66d6e56d1f
Renegade-Master <renegade.master.dev@protonmail.com>
2023-05-26 18:16:08 +0100

    Add test file structure;
    But not actual tests.

---
+++ src/jsTest/kotlin/com/renegademaster/MainTest.kt
@@ -0,0 +1,14 @@
+package com.renegademaster
+
+import kotlin.test.Test
+import kotlin.test.assertNotEquals
+
+class MainTest {
+    @Test
+    fun testHelloWorld() {
+        val a = 1
+        val b = 2
+
+        assertNotEquals(a, b)
+    }
+}
