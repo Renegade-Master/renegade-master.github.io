f2cd0cb40c09710e777687e6d03d2019be5f99c9
Renegade-Master <renegade@renegade-master.com>
2024-07-16 13:16:16 +0000

    Update Copyright Text

--- src/jsMain/kotlin/com/renegademaster/content/AboutMe.kt
+++ src/jsMain/kotlin/com/renegademaster/content/AboutMe.kt
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2023 Renegade-Master [webmaster.portly274@passfwd.com]
+ *    Copyright 2024 Renegade-Master [webmaster.portly274@passfwd.com]
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
